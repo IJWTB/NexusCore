e4bac0489b8804587cb03fda700c338d2e1bf05c
Mista-Tea <thatguythomas@hotmail.com>
2020-08-03 21:23:09 -0600

    Organized function descriptions based on usage

--- lua/entities/gmod_wire_expression2/core/custom/cl_nexuscore.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cl_nexuscore.lua
@@ -1,13 +1,19 @@
-E2Helper.Descriptions["teleport"]           = "Sets the player's position to the given vector. Requires prop protection to use on other players."
+-- Player functions
 E2Helper.Descriptions["applyPlayerForce"]   = "Applies force on the player toward the given vector. Requires prop protection or admin to use on other players."
+E2Helper.Descriptions["enableFallDamage"]   = "Sets whether the player should be affected by fall damage or not. Requires prop protection or admin to use on other players."
+E2Helper.Descriptions["teleport"]           = "Sets the player's position to the given vector. Requires prop protection to use on other players."
+
+-- Entity functions
+E2Helper.Descriptions["extinguish"]         = "Extinguishes the entity if it is on fire. Requires prop protection or admin to use on others' entities. Rate limited to 0.5 seconds."
+E2Helper.Descriptions["ignite"]             = "Sets the entity on fire with an optional time in seconds and a radius. Requires prop protection or admin to use on others' entities. Rate limited to 0.5 seconds."
 E2Helper.Descriptions["hasNoCollideAll"]    = "Returns whether the entity currently has nocollide-all applied."
 E2Helper.Descriptions["setNoCollideAll"]    = "Enables or disables nocollide-all on the given entity. Requires prop protection or admin to use on other players."
-E2Helper.Descriptions["setOwner"]           = "Changes the entity's owner to the given player. Requires admin or singleplayer to use."
-E2Helper.Descriptions["ignite"]             = "Sets the entity on fire with an optional time in seconds and a radius. Requires prop protection or admin to use on others' entities. Rate limited to 0.5 seconds."
-E2Helper.Descriptions["extinguish"]         = "Extinguishes the entity if it is on fire. Requires prop protection or admin to use on others' entities. Rate limited to 0.5 seconds."
-E2Helper.Descriptions["setHealth"]          = "Sets the entity's health to the given number. Requires prop protection to use on others' entities."
-E2Helper.Descriptions["takeDamage"]         = "Applies the given damage to the entity. Requires prop protection to use on others' entities."
 E2Helper.Descriptions["set"]                = "Fires an entity's input. Requires admin or singleplayer to use."
+E2Helper.Descriptions["setHealth"]          = "Sets the entity's health to the given number. Requires prop protection to use on others' entities."
+E2Helper.Descriptions["setOwner"]           = "Changes the entity's owner to the given player. Requires admin or singleplayer to use."
+E2Helper.Descriptions["takeDamage"]         = "Applies the given damage to the entity. Requires prop protection to use on others' entities."
+
+-- Animation functions
 E2Helper.Descriptions["animate"]            = "Resets the entity's current animation sequence with the passed numeric sequence or animation name. Requires prop protection or admin to use on others' entities."
 E2Helper.Descriptions["getSequence"]        = "Gets the entity's current numeric animation sequence."
 E2Helper.Descriptions["getAnimation"]       = "Gets the entity's current numeric animation sequence (alias of getSequence)."
@@ -15,6 +21,7 @@ E2Helper.Descriptions["lookupSequence"]     = "Gets the entity's numeric animati
 E2Helper.Descriptions["getAnimationByName"] = "Gets the entity's numeric animation sequence from the animation name (alias of lookupSequence)."
 E2Helper.Descriptions["getSequenceList"]    = "Gets the list of animation sequences available on the entity."
 E2Helper.Descriptions["sequenceDuration"]   = "Gets the duration of the entity's current sequence, or the duration of the passed sequence."
-E2Helper.Descriptions["enableFallDamage"]   = "Sets whether the player should be affected by fall damage or not. Requires prop protection or admin to use on other players."
+
+-- EGP functions
 E2Helper.Descriptions["egpHudTogglePlayer"] = "Sets whether the given player can see the Wire EGP HUD or not. Requires prop protection and PlayerUse to use on other players."
 E2Helper.Descriptions["egpHudToggle"]       = "Toggles the visibility of the player's Wire EGP HUD on themselves."
